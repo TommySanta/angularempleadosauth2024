ee457495dad2aa4d9f59527c5750848be274f82d
serraguti <pacoserranox@gmail.com>
2024-11-12 13:22:23 +0100

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { SubordinadosComponent } from './components/subordinados/subordinados.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent},
+  {path: "login", component: LoginComponent},
+  {path: "perfil", component: PerfilComponent},
+  {path: "subordinados", component: SubordinadosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
